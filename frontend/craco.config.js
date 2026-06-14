7ac30ec57052600ecabb7669a26b7f4fac798e57
RAJ PATEL <rajmacbook2024@gmail.com>
2026-06-15 02:26:50 +0530

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -7,86 +7,83 @@ let healthPluginInstance = null;
 let setupHealthEndpoints = null;
 
 if (enableHealthCheck) {
-try {
-const WebpackHealthPlugin = require("./plugins/health-check/webpack-health-plugin");
-setupHealthEndpoints = require("./plugins/health-check/health-endpoints");
-healthPluginInstance = new WebpackHealthPlugin();
-} catch (err) {
-console.warn("[health-check] Health check plugin not available:", err.message);
-}
+  try {
+    const WebpackHealthPlugin = require("./plugins/health-check/webpack-health-plugin");
+    setupHealthEndpoints = require("./plugins/health-check/health-endpoints");
+    healthPluginInstance = new WebpackHealthPlugin();
+  } catch (err) {
+    console.warn(
+      "[health-check] Health check plugin not available:",
+      err.message
+    );
+  }
 }
 
 module.exports = {
-eslint: {
-configure: {
-extends: ["plugin:react-hooks/recommended"],
-rules: {
-"react-hooks/rules-of-hooks": "error",
-"react-hooks/exhaustive-deps": "warn",
-},
-},
-},
+  eslint: {
+    configure: {
+      extends: ["plugin:react-hooks/recommended"],
+      rules: {
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+      },
+    },
+  },
 
-webpack: {
-alias: {
-"@": path.resolve(__dirname, "src"),
-},
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
 
-```
-configure: (config) => {
-  config.watchOptions = {
-    ...config.watchOptions,
-    ignored: [
-      "**/node_modules/**",
-      "**/.git/**",
-      "**/build/**",
-      "**/dist/**",
-      "**/coverage/**",
-    ],
-  };
+    configure: (config) => {
+      config.watchOptions = {
+        ...config.watchOptions,
+        ignored: [
+          "**/node_modules/**",
+          "**/.git/**",
+          "**/build/**",
+          "**/dist/**",
+          "**/coverage/**",
+        ],
+      };
 
-  if (enableHealthCheck && healthPluginInstance) {
-    config.plugins.push(healthPluginInstance);
-  }
+      if (enableHealthCheck && healthPluginInstance) {
+        config.plugins.push(healthPluginInstance);
+      }
 
-  return config;
-},
-```
+      return config;
+    },
+  },
 
-},
+  devServer: (devServerConfig) => {
+    if (
+      enableHealthCheck &&
+      setupHealthEndpoints &&
+      healthPluginInstance
+    ) {
+      const originalSetupMiddlewares =
+        devServerConfig.setupMiddlewares;
 
-devServer: (devServerConfig) => {
-if (
-enableHealthCheck &&
-setupHealthEndpoints &&
-healthPluginInstance
-) {
-const originalSetupMiddlewares =
-devServerConfig.setupMiddlewares;
-
-```
-  devServerConfig.setupMiddlewares = (
-    middlewares,
-    devServer
-  ) => {
-    if (originalSetupMiddlewares) {
-      middlewares = originalSetupMiddlewares(
+      devServerConfig.setupMiddlewares = (
         middlewares,
         devServer
-      );
+      ) => {
+        if (originalSetupMiddlewares) {
+          middlewares = originalSetupMiddlewares(
+            middlewares,
+            devServer
+          );
+        }
+
+        setupHealthEndpoints(
+          devServer,
+          healthPluginInstance
+        );
+
+        return middlewares;
+      };
     }
 
-    setupHealthEndpoints(
-      devServer,
-      healthPluginInstance
-    );
-
-    return middlewares;
-  };
-}
-
-return devServerConfig;
-```
-
-},
+    return devServerConfig;
+  },
 };
